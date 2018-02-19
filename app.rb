8dc0d6b15c6b84668c9010c3e448e0e70315e716
Ivan Szebenszki <szib@users.noreply.github.com>
2018-02-19 19:36:33 +0000

    Implement guessing

--- app.rb
+++ app.rb
@@ -19,6 +19,8 @@ get '/' do
   msg = "No more attempt... You lost." unless session[:hangman].has_more_attempt?
   msg = "Grats. You won." unless session[:hangman].not_win?
 
+  session[:hangman].guess(params['guess']) if params['guess'] =~ /^([a-z]|[A-Z])$/
+
   puts session.inspect
   erb :index, locals: { word: '_ _ _ _ _ _ D _ _', msg: msg, turns_left: 'chalkboard', incorrect_letters: 'a,b,c,s' }
 end
